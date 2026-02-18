b0f3c9ed70bb7b45dac5d69e5b2de95d65be2d38
saltrias <thecheeseplateau@gmail.com>
2026-02-18 22:31:45 +0700

    packetfall effect
    id like to call this the packetfall effect i just took /dev/urandom and displayed it like a waterfall

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ const params = new URLSearchParams(window.location.search);
 
 // Check if beta param exists
 if (!(params.has("beta"))) {
-    window.location.href = "https://saltrias.github.io/stopwatch"; 
+    window.location.href = "stopwatch/"; 
 } 
+
+const packetEl = document.getElementById("packet-text");
+
+async function loadChars() {
+    const res = await fetch("random.txt");
+    const randomChars = await res.text();
+    packetEl.innerText = randomChars
+}
+
+loadChars()
