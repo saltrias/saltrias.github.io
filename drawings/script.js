002b48cf0b0af54a5fd46755c096efd0de631eb1
al <thecheeseplateau@gmail.com>
2026-03-24 21:13:34 +0700

    clean slate vII

--- drawings/script.js
+++
@@ -1,8 +0,0 @@
-const gallery = document.getElementById("gallery");
-let images = 24;
-
-for (let i = images; i > 0; i--) {
-    let img = document.createElement("img")
-    img.src = `images/${i}.png`;
-    gallery.appendChild(img);
-}
