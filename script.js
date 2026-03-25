3034dbf37e0ac021bd3ca813c1b8026d4d1067dd
al <thecheeseplateau@gmail.com>
2026-03-25 20:45:29 +0700

    Whoa

---
+++ script.js
@@ -0,0 +1,15 @@
+const randomDrawingEl = document.getElementById("randomDrawing");
+const randomButtonEl = document.getElementById("randomizeDrawing");
+
+const DRAWINGS = 24;
+
+function changeImage(id) {
+    randomDrawingEl.src = `assets/drawings/${id}.png`
+    randomDrawingEl.classList = `${id}`
+}
+
+randomButtonEl.addEventListener("click", () => {
+    changeImage(Math.floor(Math.random() * DRAWINGS) + 1)
+})
+
+changeImage(12);
